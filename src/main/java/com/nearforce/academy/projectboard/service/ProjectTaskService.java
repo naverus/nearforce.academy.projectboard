41ff59e95746ec96f5fbd19c7ddb706b1d4894d0
Navor Nunez <navor.nunez@solardesigntool.com>
2021-04-13 20:14:19 -0400

    add GetAll and GetById.

--- src/main/java/com/nearforce/academy/projectboard/service/ProjectTaskService.java
+++ src/main/java/com/nearforce/academy/projectboard/service/ProjectTaskService.java
@@ -4,10 +4,12 @@ import com.nearforce.academy.projectboard.domain.ProjectTask;
 import com.nearforce.academy.projectboard.repository.ProjectTaskRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ProjectTaskService {
 
-    private ProjectTaskRepository projectTaskRepository;
+    private final ProjectTaskRepository projectTaskRepository;
 
     ProjectTaskService(ProjectTaskRepository projectTaskRepository) {
         this.projectTaskRepository = projectTaskRepository;
@@ -17,6 +19,14 @@ public class ProjectTaskService {
         if (projectTask.getStatus() == null || projectTask.getStatus().equals("")) {
             projectTask.setStatus("TO_DO");
         }
-            return projectTaskRepository.save(projectTask);
+        return projectTaskRepository.save(projectTask);
+    }
+
+    public Iterable<ProjectTask> findAll() {
+        return projectTaskRepository.findAll();
+    }
+
+    public Optional<ProjectTask> findById(Long id) {
+        return projectTaskRepository.findById(id);
     }
 }
--- src/main/java/com/nearforce/academy/projectboard/web/ProjectTaskController.java
+++ src/main/java/com/nearforce/academy/projectboard/web/ProjectTaskController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -35,6 +38,20 @@ public class ProjectTaskController {
             return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
         }
         ProjectTask resultProjectTask = projectTaskService.saveOrUpdateProjectTask(projectTask);
-        return new ResponseEntity<ProjectTask>(resultProjectTask, HttpStatus.CREATED);
+        return new ResponseEntity<>(resultProjectTask, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public Iterable<ProjectTask> getAllProjectTasks() {
+        return projectTaskService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProjectTaskById(@PathVariable Long id) {
+        Optional<ProjectTask> result = projectTaskService.findById(id);
+        if (result.isEmpty()) {
+            return new ResponseEntity<>("Project not found with id " + id + ". ", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(result.get(), HttpStatus.OK);
     }
 }
